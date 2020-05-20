dc9597e2a204c06ab66df50d4c1e585b4f382098
sapan-jena <sapan-jena@outlook.com>
2020-05-20 10:31:38 +0530

    add daycare website

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+var model = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == model) {
+        model.style.display = "none";
+    }
+}
